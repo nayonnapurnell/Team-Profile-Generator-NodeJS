7ca889958aa89d019a0fda11d139c2ce0187e726
“Nayonna <Nayonna Purnell>
2022-11-21 23:04:32 -0500

    Created the User Input Prompt shell

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const inquirer = require('inquirer');
 const Employee = require('./employee');
+
+const promptUser = () => {
+    return inquirer.prompt([
+
+    ]);
+};
+
+const init = () => {
+    promptUser()
+      //.then((answers) => writeFile('generateMarkdown.md', generateMarkdown(answers)))
+      .then(() => console.log('Successfully wrote to html file'))
+      .catch((err) => console.error(err));
+  };
+  
+  init();
