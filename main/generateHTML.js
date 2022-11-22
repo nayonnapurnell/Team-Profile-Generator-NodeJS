2be02111fa29c074863fbf7a125f8f88d28d2bbe
“Nayonna <Nayonna Purnell>
2022-11-22 08:57:09 -0500

    Added the bootstrap cards

---
+++ main/generateHTML.js
@@ -0,0 +1,28 @@
+function generateHTML({ id, name, email, role }) {
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <title>Document</title>
+    </head>
+    <body>
+    
+    <h1 class="fs-1">My Team</h1>
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+            <div class="card-header">${name}</div>
+            <div class="card-body text-success">
+            <h5 class="card-title">${role}</h5>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${id}</li>
+            <li class="list-group-item">Email: ${email}</li>
+            <li class="list-group-item">Role: ${role}</li>
+            </ul>
+            </div>
+        </div>
+    </body>
+</html>`
+}
+
+module.exports = generateHTML;
---
+++ main/index.js
@@ -0,0 +1,39 @@
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+let generateHTML = require('./generateHTML.js');
+//const Employee = require('./employee');
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the Employee ID?',
+          },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the Employee Name?',
+          },
+          {
+            type: 'input',
+            name: 'email',
+            message: 'What is the Employee Email Address?',
+          },
+          {
+            type: 'input',
+            name: 'role',
+            message: 'What is the Employee Role?',
+          },
+
+    ]);
+};
+
+const init = () => {
+    promptUser()
+      .then((answers) => writeFile('index.html', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to index.html'))
+      .catch((err) => console.error(err));
+  };
+  
+  init();
