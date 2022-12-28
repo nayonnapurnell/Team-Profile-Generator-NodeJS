3efd614477fb8eb51d2903c8d49789d8a6c86596
“Nayonna <Nayonna Purnell>
2022-12-28 00:32:48 -0500

    Added the intern test class

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -50,9 +50,6 @@ describe("Engineer", () => {
       
             expect(eng).toThrowError(err);
           });
-
-
-
     });
     
 })
---
+++ tests/Intern.test.js
@@ -0,0 +1,55 @@
+const Intern = require("../intern");
+
+describe("Intern", () => {
+    describe("Initialization", ()=> {
+        it("should create an object with a name and age if provided valid arguments", () => {
+            const intern = new Intern(003, "Alisah", "Alisah.tester@gmail.com", "Temple University");
+      
+            // Verify that the new object has the correct properties
+            expect(intern.id).toEqual(003);
+            expect(intern.name).toEqual("Alisah");
+            expect(intern.email).toEqual("Alisah.tester@gmail.com");
+            expect(intern.school).toEqual("Temple University");
+          });
+
+          it("should throw an error if provided no arguments", () => {
+            // Wrap the object initialization in a callback function that Jest will run
+            const int = () => new Intern();
+      
+            // Verify that an error was thrown in the callback function
+            expect(int).toThrow();
+          });
+
+          it("should throw an error if not provided an age", () => {
+            const int = () => new Intern("Alisah");
+      
+            // Define the error message that is expected to be thrown
+            const err = new Error("Expected parameter 'id' to be a number");
+      
+            // Verify that the correct error was thrown when the callback is executed
+            expect(int).toThrowError(err);
+          });
+
+          it("should throw an error if 'name' is not a string", () => {
+            const int = () => new Intern(3, 2);
+            const err = new Error("Expected parameter 'name' to be a non-empty string");
+      
+            expect(int).toThrowError(err);
+          });
+
+          it("should throw an error if 'email' is not a string", () => {
+            const int = () => new Intern(3, 2);
+            const err = new Error("Expected parameter 'email' to be a non-empty string");
+      
+            expect(int).toThrowError(err);
+          });
+
+          it("should throw an error if 'school' is not a string", () => {
+            const int = () => new Intern(3, 2);
+            const err = new Error("Expected parameter 'school' to be a non-empty string");
+      
+            expect(int).toThrowError(err);
+          });
+    });
+    
+})
