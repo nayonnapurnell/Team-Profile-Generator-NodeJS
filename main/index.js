33cc1e31b435b38df884cdd029b223baa8d14c62
“Nayonna <Nayonna Purnell>
2023-01-03 00:55:55 -0500

    Added the functioning Employee Profile Card generator

--- main/index.js
+++ main/index.js
@@ -1,4 +1,3 @@
-let generateHTML = require('./generateHTML.js');
 let Employee = require('../lib/employee');
 let Engineer = require('../lib/engineer');
 let Manager = require('../lib/manager');
@@ -61,22 +60,25 @@ const addATeam = () => {
       message: 'Do you want to add another profile?',
     },
   ]).then((answers) => {
-    if(answers.profile === true){
-      init();
+    if(answers.profile === false){
+      print();
     }
     else{
-      writeFile('index.html', generateHTML(profileList))
-      console.log(answers);
-      console.log(profileList);
-      console.log('Successfully wrote to index.html');
+      init();
     }
   }).catch((err) => console.error(err));  
 };
 
+
+
+
+
+
+
 const init = () => {
   addAProfile().then((answers) => { 
     if(answers.role === "Manager"){      
-      profileList.push(new Manager(answers.id, answers.name, answers.email, answers.officeNumber));
+      profileList.push(new Manager(answers.id, answers.name, answers.email, answers.officeNumber, answers.role));
     }
       if(answers.role === "Engineer"){
       profileList.push(new Engineer(answers.id, answers.name, answers.email, answers.github));
@@ -85,10 +87,81 @@ const init = () => {
       profileList.push(new Intern(answers.id,answers.name, answers.email, answers.school));
     }
     addATeam();
+    
   })
       .catch((err) => console.error(err));  
   };
   
   init();
 
+  const print = () => {
+    let employeeProfileData =
+    profileList.map( answers => {
+      if(answers.getRole() === "Manager"){
+        return `    
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header name">${answers.name}</div>
+        <div class="card-body text-primary">
+        <h5 class="card-title text-white"><i class="fa-solid fa-mug-saucer"></i> Manager</h5>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item id">ID:${answers.id}</li>
+        <li class="list-group-item email">Email: ${answers.email}</li>
+        <li class="list-group-item role">Role: ${answers.officeNumber}</li>
+        </ul>
+        </div>
+        </div>
+        </div>`
+      }
+      if(answers.role === "Engineer"){
+        return `
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header name">${answers.name}</div>
+        <div class="card-body text-primary">
+        <h5 class="card-title text-white">Engineer</h5>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item id">ID:${answers.id}</li>
+        <li class="list-group-item email">Email: ${answers.email}</li>
+        <li class="list-group-item role">Role: ${answers.github}</li>
+        </ul>
+        </div>
+        </div>
+        </div>`;
+      }
+      if(answers.role === "Intern"){
+        return `
+        <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
+        <div class="card-header name">${answers.name}</div>
+        <div class="card-body text-primary">
+        <h5 class="card-title text-white">"Intern</h5>
+        <ul class="list-group list-group-flush">
+        <li class="list-group-item id">ID:${answers.id}</li>
+        <li class="list-group-item email">Email: ${answers.email}</li>
+        <li class="list-group-item role">Role: ${answers.school}</li>
+        </ul>
+        </div>
+        </div>
+        </div>`;
+      }
+    }).join("");
+    const data = 
+    `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <title>Document</title>
+    </head>
+    <body>
+    <h1 class="fs-1">My Team</h1>
+      <ul class="employee-list">
+        ${employeeProfileData}
+      </ul>
+    </body>
+    </html>
+    `
+writeFile('index.html', data)
+console.log('Successfully wrote to index.html');
+  }
  
